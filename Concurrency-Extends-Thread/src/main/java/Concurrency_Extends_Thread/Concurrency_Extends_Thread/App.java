e64594269d00ac588f5ca779f6478abf00f9a1d8
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-17 00:08:08 -0600

    Concurrency extends

---
+++ Concurrency-Extends-Thread/src/main/java/Concurrency_Extends_Thread/Concurrency_Extends_Thread/App.java
@@ -0,0 +1,57 @@
+package Concurrency_Extends_Thread.Concurrency_Extends_Thread;
+
+
+/**
+ * Hello world!
+ *
+ */
+class runner1 extends Thread{
+
+	@Override
+	public void run() {
+		for(int i = 0; i <10; i++)
+		{
+			System.out.println("Runner 1 <>" + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	
+}
+
+class runner2 extends Thread{
+
+	@Override
+	public void run() {
+		for(int i = 0; i <10; i++)
+		{
+			System.out.println("Runner 2 <>" + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+}
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Thread t1 = new Thread(new runner1());
+    	Thread t2 = new Thread(new runner2());
+
+    	t1.start();
+    	t2.start();
+
+    }
+}

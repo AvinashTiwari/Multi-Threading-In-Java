d8aceaced1ad29fbb4b5b5559b4b59688e42feac
Avinash Tiwari <qwe123kids@gmail.com>
2018-04-01 15:10:40 -0600

    Chopstik implementation

--- Dining-Philosopher/src/main/java/Dining_Philosopher/Dining_Philosopher/ChopSticks.java
+++ Dining-Philosopher/src/main/java/Dining_Philosopher/Dining_Philosopher/ChopSticks.java
@@ -1,5 +1,38 @@
 package Dining_Philosopher.Dining_Philosopher;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ChopSticks {
+	private int id;
+	private Lock lock;
+
+	public ChopSticks(int id) {
+		this.id = id;
+		lock = new ReentrantLock();
+	}
+
+	public boolean pickUp(Philosopher philosopher, State state) throws InterruptedException {
+		if (lock.tryLock(10, TimeUnit.MILLISECONDS)) {
+			System.out.println(philosopher + " pickedup " + state.toString() + " " + this);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public void putDown(Philosopher philosopher, State state)
+	{
+		lock.unlock();
+		System.out.println(philosopher + " put down "  + this);
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "chopStick " + id;
+	}
 
 }

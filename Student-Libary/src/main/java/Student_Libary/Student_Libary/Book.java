6a61c29035af450d72a14f83ceb8da016584f9d7
Avinash Tiwari <qwe123kids@gmail.com>
2018-04-03 21:47:58 -0600

    Student Libary

--- Student-Libary/src/main/java/Student_Libary/Student_Libary/Book.java
+++ Student-Libary/src/main/java/Student_Libary/Student_Libary/Book.java
@@ -1,5 +1,38 @@
 package Student_Libary.Student_Libary;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Book {
+	private int id;
+	private Lock lock;
+	
+	public Book(int id){
+		this.id = id;
+		
+		lock = new ReentrantLock();
+	}
+	
+	public void read(Student student) throws InterruptedException
+	{
+		lock.tryLock(10, TimeUnit.MILLISECONDS);
+		System.out.println(student + " Start Readin " + this);
+		Thread.sleep(2000);
+		lock.unlock();
+		System.out.println(student + " Finished reading " + this);
+
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Book #"+id;
+	}
+	  
+	
+	
+	
 
 }
--- Student-Libary/src/main/java/Student_Libary/Student_Libary/Student.java
+++ Student-Libary/src/main/java/Student_Libary/Student_Libary/Student.java
@@ -1,5 +1,38 @@
 package Student_Libary.Student_Libary;
 
-public class Student {
+import java.util.Random;
 
+public class Student implements Runnable{
+	private int id;
+	
+	private Book[] book;
+	
+	public Student(int id, Book[] book)
+	{
+		this.id = id;
+		this.book = book;
+	}
+
+	public void run() {
+		Random random = new Random();
+		while(true)
+		{
+			int bookId = random.nextInt(Constant.NUMBER_OF_BOOKS);
+			try {
+				book[bookId].read(this);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Student # " +id;
+	}
+	
+	
 }

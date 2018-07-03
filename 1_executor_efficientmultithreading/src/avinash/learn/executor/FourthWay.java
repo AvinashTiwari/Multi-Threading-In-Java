5784b793dcd5cc0b325dd6e7e73358b61b43ce56
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-02 20:11:04 -0600

    no message

---
+++ 1_executor_efficientmultithreading/src/avinash/learn/executor/FourthWay.java
@@ -0,0 +1,36 @@
+package avinash.learn.executor;
+
+import java.util.concurrent.TimeUnit;
+
+public class FourthWay {
+	public static void main(String[] args) {
+		System.out.println("main thread starts here");
+		new Thread(new FourthTask()).start();;
+		Thread t = new Thread(new FourthTask());
+        t.start();
+		System.out.println("main thread ends here");
+
+	}
+}
+
+class FourthTask implements Runnable {
+	private static int count = 0;
+	private int id;
+	@Override
+	public void run() {
+		for (int i = 10; i > 0; i--) {
+			System.out.println("< " + id +" > Tick " + i);
+			try {
+				TimeUnit.MILLISECONDS.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public FourthTask() {
+		this.id  = count++;
+//		new Thread(this).start();
+	}
+}

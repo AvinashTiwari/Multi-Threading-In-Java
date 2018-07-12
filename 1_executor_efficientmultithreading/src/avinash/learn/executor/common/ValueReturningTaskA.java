16dd9dfe0b5064a5a64bfd1fc3491f6130558efb
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-11 22:24:03 -0600

    no message

--- 1_executor_efficientmultithreading/src/avinash/learn/executor/common/ValueReturningTaskA.java
+++ 1_executor_efficientmultithreading/src/avinash/learn/executor/common/ValueReturningTaskA.java
@@ -1,28 +1,44 @@
 package avinash.learn.executor.common;
 
+import java.util.concurrent.TimeUnit;
+
 public class ValueReturningTaskA implements Runnable{
 
 	private int a;
 	private int b;
-	private long sleep; 
+	private long sleepTime; 
 	private int sum;
 	
 	private static int count =0;
 	private int instanceNumber;
 	private String taskId;
 	
-	public ValueReturningTaskA(int a , int  b, long sleep) {
+	public ValueReturningTaskA(int a , int  b, long sleepTime) {
 		this.a =a;
 		this.b =b;
-		this.sleep = sleep;
+		this.sleepTime = sleepTime;
 		this.instanceNumber = ++count;
 		this.taskId = "ValueReturnId " + this.instanceNumber;
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		String currentThreadName = Thread.currentThread().getName();
+		System.out.println("###[ " + currentThreadName + " ]##  < " + taskId + " > Starting");
+		System.out.println(" [ " + currentThreadName + " ] sleeping for " +  sleepTime);
+		try {
+			TimeUnit.MILLISECONDS.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		sum = a+b ;
+		System.out.println("**** [ " + currentThreadName + " ] < " + taskId + ">");
+	}
+	
+	public int getSum(){
+		return sum;
 	}
 
 }
--- 1_executor_efficientmultithreading/src/avinash/learn/executor/returnvalue/ReturningValueFirstWay.java
+++ 1_executor_efficientmultithreading/src/avinash/learn/executor/returnvalue/ReturningValueFirstWay.java
@@ -1,11 +1,28 @@
 package avinash.learn.executor.returnvalue;
 
+import avinash.learn.executor.common.ValueReturningTaskA;
+
 public class ReturningValueFirstWay {
 
 	public static void main(String[] args) {
     String currentThreadName = Thread.currentThread().getName();
     System.out.println("[ " + currentThreadName + " ] Main Thread starts Here");
+		ValueReturningTaskA  task1 = new ValueReturningTaskA(2,3,2000);
+		Thread t1 = new Thread(task1 , "Thread 1");
 		
+		ValueReturningTaskA  task2 = new ValueReturningTaskA(3,4,1000);
+		Thread t2 = new Thread(task2 , "Thread 2");
+		
+		ValueReturningTaskA  task3 = new ValueReturningTaskA(4,5,500);
+		Thread t3 = new Thread(task3 , "Thread 3");
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		System.out.println("T1 " + task1.getSum());
+		System.out.println("T2 " + task2.getSum());
+		System.out.println("T3 " + task3.getSum());
+
     System.out.println("[ " + currentThreadName + " ] Main Thread Ends Here");
 
 	}

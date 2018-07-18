f258d719fbc3ed203a2e4219dd931eeb09c6e773
Avinash Tiwari <qwe123kids@gmail.com>
2018-07-17 22:33:15 -0600

    no message

--- 1_executor_efficientmultithreading/src/avinash/learn/executor/daemon/DaemonThreadExecutor.java
+++ 1_executor_efficientmultithreading/src/avinash/learn/executor/daemon/DaemonThreadExecutor.java
@@ -1,7 +1,9 @@
 package avinash.learn.executor.daemon;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import avinash.learn.executor.common.DaemonThreadFactory;
 import avinash.learn.executor.common.LoopTaskD;
 
 public class DaemonThreadExecutor {
@@ -9,7 +11,15 @@ public class DaemonThreadExecutor {
 	public static void main(String[] args) {
 		  String currentThreadName = Thread.currentThread().getName();
 		    System.out.println("[ " + currentThreadName + " ] Main Thread starts Here");
-			
+		    
+		    ExecutorService  exeService = Executors.newCachedThreadPool(new DaemonThreadFactory());
+		    exeService.equals(new  LoopTaskD(100));
+		    exeService.equals(new  LoopTaskD(200));
+		    exeService.equals(new  LoopTaskD(100));
+		    exeService.equals(new  LoopTaskD(200));
+		    
+		    exeService.shutdown();
+
 		    System.out.println("[ " + currentThreadName + " ] Main Thread ends Here");
 
 	}
